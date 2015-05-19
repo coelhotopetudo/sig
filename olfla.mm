1b9948315f0dff991f455ea8768c36ddbf920b42
Alisson Coelho de Morais - SUPDE/DECTA/DE909 <00737990929@serpro-1467148.(none)>
2015-05-19 08:11:22 -0300

    Primeira versao

---
+++ olfla.mm
@@ -0,0 +1,221 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1366484786462" ID="ID_1390590066" LINK="olflaTreinamento.mm" MODIFIED="1366889027408" STYLE="bubble" TEXT="impresso">
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<node CREATED="1366583179441" FOLDED="true" ID="ID_345280446" MODIFIED="1366891960705" POSITION="right" STYLE="bubble" TEXT="Sobre o material e treinamento">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1366583185794" ID="ID_1733612757" MODIFIED="1366888973560" STYLE="bubble" TEXT="Impresso: Mais b&#xe1;sico e pequena refer&#xea;ncia"/>
+<node CREATED="1366296774197" ID="ID_1529591533" MODIFIED="1366888973560" STYLE="bubble" TEXT="baseado no workshop da opengeo"/>
+<node CREATED="1366586135890" ID="ID_79253440" MODIFIED="1366888973560" STYLE="bubble" TEXT="N&#xe3;o esgotar ou decorar, mas sim como utilizar a documenta&#xe7;&#xe3;o e ferramentas"/>
+<node CREATED="1366302425796" ID="ID_315503097" MODIFIED="1366888973560" STYLE="bubble" TEXT="firebug"/>
+</node>
+<node CREATED="1366484790091" ID="ID_282467437" MODIFIED="1366892005692" POSITION="right" STYLE="bubble" TEXT="Web mapping">
+<node CREATED="1366484790092" FOLDED="true" ID="ID_45298883" MODIFIED="1366894104844" TEXT="Introdu&#xe7;&#xe3;o">
+<node CREATED="1366583031776" MODIFIED="1366583035097" TEXT="O que &#xe9;"/>
+<node CREATED="1366583035799" MODIFIED="1366583070874" TEXT="webgis (processamento, an&#xe1;lise)"/>
+</node>
+<node CREATED="1366484790092" ID="ID_595323057" MODIFIED="1366894105612" TEXT="WMS Vs WFS">
+<node CREATED="1366484790092" MODIFIED="1366484790092" TEXT="onde acontece o processamento, renderiza&#xe7;&#xe3;o"/>
+<node CREATED="1366484790094" MODIFIED="1366484790094" TEXT="formatos"/>
+</node>
+<node CREATED="1366484790094" ID="ID_1708847027" MODIFIED="1366484790094" TEXT="WCS (raster)"/>
+<node CREATED="1366484790095" MODIFIED="1366484790095" TEXT="WFS-T (cria&#xe7;&#xe3;o e edi&#xe7;&#xe3;o)"/>
+</node>
+<node CREATED="1360605278406" FOLDED="true" ID="ID_207063710" MODIFIED="1366888989545" POSITION="right" STYLE="bubble" TEXT="O que &#xe9;?">
+<node CREATED="1360866668584" ID="ID_203152742" MODIFIED="1366888973561" STYLE="bubble" TEXT="Biblioteca javascript cross-browser para Web mapping"/>
+</node>
+<node CREATED="1360605146528" FOLDED="true" ID="ID_660095550" MODIFIED="1366888989546" POSITION="right" STYLE="bubble" TEXT="Por que usar?">
+<node CREATED="1360605153939" ID="ID_1268513992" MODIFIED="1366888973561" STYLE="bubble" TEXT="lib independente, armazenada no pr&#xf3;prio servidor "/>
+<node CREATED="1360866741673" FOLDED="true" ID="ID_553241907" MODIFIED="1366888973561" STYLE="bubble" TEXT="suporte a diversos protocolos">
+<node CREATED="1360866754257" MODIFIED="1360866755829" TEXT="WMS"/>
+<node CREATED="1360866756065" MODIFIED="1360866758429" TEXT="WFS"/>
+<node CREATED="1360866758665" MODIFIED="1360866763157" TEXT="WFS-T"/>
+</node>
+<node CREATED="1366594467539" FOLDED="true" ID="ID_230764227" MODIFIED="1366888989545" STYLE="bubble" TEXT="vers&#xf5;es da biblioteca">
+<node CREATED="1366594492085" ID="ID_1399819106" MODIFIED="1366888973561" STYLE="bubble" TEXT="debug"/>
+<node CREATED="1366594494311" ID="ID_961596385" MODIFIED="1366888973561" STYLE="bubble" TEXT="light"/>
+<node CREATED="1366594495752" ID="ID_83226211" MODIFIED="1366888973561" STYLE="bubble" TEXT="mobile"/>
+</node>
+<node CREATED="1354016720353" FOLDED="true" ID="ID_832995946" MODIFIED="1366888973561" STYLE="bubble" TEXT="Build enxuta">
+<node CREATED="1360605200271" MODIFIED="1361110112487" TEXT="pode ser compactada contendo somente as classes usadas na implementa&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1354028720433" ID="ID_1770240462" MODIFIED="1366888973561" STYLE="bubble" TEXT="Rasters pr&#xf3;prios"/>
+<node CREATED="1354056150909" FOLDED="true" ID="ID_1037001631" MODIFIED="1366888989545" STYLE="bubble" TEXT="Alternar entre baselayer">
+<node CREATED="1361044641669" ID="ID_1665525049" MODIFIED="1366888973561" STYLE="bubble" TEXT="OSM"/>
+<node CREATED="1361044643953" ID="ID_1862805558" MODIFIED="1366888973561" STYLE="bubble" TEXT="Bing"/>
+</node>
+<node CREATED="1354016720353" ID="ID_384791599" MODIFIED="1366888973561" STYLE="bubble" TEXT="N&#xe3;o sofre com deprecated"/>
+<node CREATED="1354016720354" FOLDED="true" ID="ID_905742119" MODIFIED="1366888973561" STYLE="bubble" TEXT="Mais facil para debugar">
+<node CREATED="1360841611192" MODIFIED="1360841626044" TEXT="js espec&#xed;fico"/>
+<node CREATED="1360841626704" MODIFIED="1360841648188" TEXT="firebug controles adicionais"/>
+</node>
+<node CREATED="1360928516751" FOLDED="true" ID="ID_635819387" MODIFIED="1366888989545" STYLE="bubble" TEXT="reproje&#xe7;&#xe3;o">
+<node CREATED="1366594529502" ID="ID_1354896474" MODIFIED="1366888973561" STYLE="bubble" TEXT="Adequar as diversas proje&#xe7;&#xf5;es de mapas (WGS 84, Web Mercator, etc)">
+<cloud/>
+</node>
+</node>
+<node CREATED="1360972548674" FOLDED="true" ID="ID_94555891" MODIFIED="1366888989545" STYLE="bubble" TEXT="Servi&#xe7;os baselayer">
+<node CREATED="1360761624208" ID="ID_1985864474" MODIFIED="1366888973561" STYLE="bubble" TEXT="tem construtores para os principais servi&#xe7;os de mapa"/>
+<node CREATED="1360761640855" ID="ID_680205069" MODIFIED="1366888973561" STYLE="bubble" TEXT="OpenStreetMaps"/>
+<node CREATED="1360761647230" ID="ID_1445734325" MODIFIED="1366888973562" STYLE="bubble" TEXT="Google Maps"/>
+<node CREATED="1360761650524" ID="ID_1137533504" MODIFIED="1366888973562" STYLE="bubble" TEXT="Bing"/>
+<node CREATED="1366585767907" FOLDED="true" ID="ID_1457147357" MODIFIED="1366888989545" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="attach"/>
+<node CREATED="1366660184433" ID="ID_715202618" MODIFIED="1366888973562" STYLE="bubble" TEXT="http://openlayers.org/dev/examples/google-v3-alloverlays.html"/>
+</node>
+</node>
+</node>
+<node CREATED="1366487380610" FOLDED="true" ID="ID_561104621" MODIFIED="1366888989546" POSITION="right" STYLE="bubble" TEXT="Documenta&#xe7;&#xe3;o">
+<node CREATED="1366487380611" FOLDED="true" ID="ID_1368617965" MODIFIED="1366888973562" STYLE="bubble" TEXT="&#x201c;Learn by examples&#x201d;">
+<node CREATED="1366487380612" LINK="http://openlayers.org/dev/examples" MODIFIED="1366487380612" TEXT="openlayers.org &gt; Dev &gt; Examples"/>
+<node CREATED="1366487380612" MODIFIED="1366487380612" TEXT="Dos desenvolvedores diferente do release"/>
+</node>
+<node CREATED="1366487380613" FOLDED="true" ID="ID_108476697" MODIFIED="1366888973562" STYLE="bubble" TEXT="API">
+<node CREATED="1366487380613" LINK="http://dev.openlayers.org/releases/OpenLayers-2.12/doc/apidocs/files/OpenLayers-js.html" MODIFIED="1366487380613" TEXT="dev.openlayers.org &gt; Releases &gt; OpenLayers-2.12 &gt; Doc &gt; Apidocs &gt; Files &gt; OpenLayers-js"/>
+</node>
+<node CREATED="1366487380615" FOLDED="true" ID="ID_1492953918" MODIFIED="1366888989546" STYLE="bubble" TEXT="User">
+<node CREATED="1366487380615" ID="ID_136058495" LINK="http://trac.openlayers.org/wiki/Documentation" MODIFIED="1366888973562" STYLE="bubble" TEXT="trac.openlayers.org &gt; Wiki &gt; Documentation"/>
+</node>
+<node CREATED="1365979303341" FOLDED="true" ID="ID_1981230351" MODIFIED="1366888989546" STYLE="bubble" TEXT="Proxy HTTP">
+<node CREATED="1365979313262" ID="ID_337306247" MODIFIED="1366888973562" STYLE="bubble" TEXT="porque pode precisar"/>
+<node CREATED="1366652222470" ID="ID_1142456156" MODIFIED="1366888973562" STYLE="bubble" TEXT="WFS"/>
+<node CREATED="1366652225686" ID="ID_574277635" MODIFIED="1366888973562" STYLE="bubble" TEXT="Restri&#xe7;&#xf5;es de seguran&#xe7;a do Javascript com opera&#xe7;&#xf5;es XMLHttpRequest a outros dom&#xed;nios"/>
+<node CREATED="1366652271591" ID="ID_1990064610" MODIFIED="1366888973562" STYLE="bubble" TEXT="No site tem como fazer para funcionar"/>
+<node CREATED="1365979320560" FOLDED="true" ID="ID_490889577" MODIFIED="1366888989546" STYLE="bubble" TEXT="Why do I need a ProxyHost?">
+<node CREATED="1366652196407" ID="ID_896320664" MODIFIED="1366888973562" STYLE="bubble" TEXT="http://trac.osgeo.org/openlayers/wiki/FrequentlyAskedQuestions#ProxyHost"/>
+</node>
+</node>
+<node CREATED="1366584847908" FOLDED="true" ID="ID_859345971" MODIFIED="1366888989546" STYLE="bubble" TEXT="Pesquisar um exemplo no site">
+<node CREATED="1366487380612" ID="ID_377693264" LINK="http://openlayers.org/dev/examples" MODIFIED="1366888973563" STYLE="bubble" TEXT="openlayers.org &gt; Dev &gt; Examples"/>
+</node>
+</node>
+<node CREATED="1366487399412" FOLDED="true" ID="ID_1722407946" MODIFIED="1366888989547" POSITION="right" STYLE="bubble" TEXT="Mapa b&#xe1;sico">
+<node CREATED="1366487399412" FOLDED="true" ID="ID_91323227" MODIFIED="1366888973563" STYLE="bubble" TEXT="Importar biblioteca">
+<node CREATED="1366487399413" MODIFIED="1366487399413" TEXT="&lt;script src=&quot;openlayers/lib/OpenLayers.js&quot;&gt;&lt;/script&gt;"/>
+</node>
+<node CREATED="1366487399414" FOLDED="true" ID="ID_123114269" MODIFIED="1366888973563" STYLE="bubble" TEXT="Localiza&#xe7;&#xe3;o do mapa">
+<node CREATED="1366487399415" MODIFIED="1366487399415" TEXT="&lt;div id=&quot;map-id&quot;&gt;&lt;/div&gt;"/>
+</node>
+<node CREATED="1366487399415" FOLDED="true" ID="ID_1587886170" MODIFIED="1366888989547" STYLE="bubble" TEXT="Configura&#xe7;&#xe3;o mapa (sempre depois div)">
+<node CREATED="1366584990693" ID="ID_1683025600" MODIFIED="1366888973563" STYLE="bubble" TEXT="Precisa ser depois para que a biblioteca encontre onde colocar o conte&#xfa;do (pode ser no onLoad do body tamb&#xe9;m)">
+<cloud/>
+</node>
+<node CREATED="1366487399415" ID="ID_611518788" MODIFIED="1366888973565" STYLE="bubble" TEXT="&lt;script&gt;var map = new OpenLayers.Map(&quot;map-id&quot;);"/>
+<node CREATED="1366487399416" ID="ID_1503951733" MODIFIED="1366888973565" STYLE="bubble" TEXT="// implementa&#xe7;&#xe3;o"/>
+<node CREATED="1366487399417" ID="ID_610968106" MODIFIED="1366888973565" STYLE="bubble" TEXT="&lt;/script&gt;"/>
+</node>
+</node>
+<node CREATED="1366487409022" FOLDED="true" ID="ID_366028626" MODIFIED="1366888973565" POSITION="right" STYLE="bubble" TEXT="Conceitos - Camadas">
+<node CREATED="1366487409022" MODIFIED="1366487409022" TEXT="Baselayer (OSM, Google Maps, raster)"/>
+<node CREATED="1366487409023" FOLDED="true" ID="ID_1332020691" MODIFIED="1366888989547" TEXT="Overlay (WMS, raster)">
+<node CREATED="1366487409023" MODIFIED="1366487409023" TEXT="Transpar&#xea;ncia"/>
+<node CREATED="1366487409023" MODIFIED="1366487409023" TEXT="Escala"/>
+</node>
+<node CREATED="1366487409024" FOLDED="true" ID="ID_435225967" MODIFIED="1366888989547" TEXT="Vector (WFS, pr&#xf3;pria)">
+<node CREATED="1366585104188" MODIFIED="1366651475123" TEXT="Pr&#xf3;pria, criada pelo pr&#xf3;prio OpenLayers em tempo de execu&#xe7;&#xe3;o"/>
+<node CREATED="1366487409024" MODIFIED="1366487409024" TEXT="Simbologia"/>
+<node CREATED="1366487409024" MODIFIED="1366487409024" TEXT="Escala"/>
+</node>
+<node CREATED="1366166600122" FOLDED="true" ID="ID_1293000598" MODIFIED="1366888989547" TEXT="ordem das camadas">
+<node CREATED="1366166610386" MODIFIED="1366166620415" TEXT="pontos e linhas Vs poligonos"/>
+<node CREATED="1366587037968" MODIFIED="1366587051946" TEXT="pode-se alterar a ordem depois"/>
+<node CREATED="1366594149083" MODIFIED="1366652629215" TEXT="Map-js.html#OpenLayers.Map.setLayerIndex">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node CREATED="1354016577706" FOLDED="true" ID="ID_999323933" MODIFIED="1366888989547" TEXT="Exemplo">
+<node CREATED="1365980220786" MODIFIED="1366586731496" TEXT="O foco dos exemplos n&#xe3;o &#xe9; beleza, mas sim as funcionalidades do OpenLayers.">
+<cloud/>
+</node>
+<node CREATED="1360965720997" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/dwfs-states.html" MODIFIED="1360965720997" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Dwfs-states"/>
+</node>
+</node>
+<node CREATED="1366487421809" FOLDED="true" ID="ID_1938335204" MODIFIED="1366888989548" POSITION="left" STYLE="bubble" TEXT="Conceitos - Controles">
+<node CREATED="1366487421809" FOLDED="true" ID="ID_461215665" MODIFIED="1366888989547" STYLE="bubble" TEXT="Selecionar/destacar features">
+<node CREATED="1366585322211" ID="ID_335354148" MODIFIED="1366888973565" STYLE="bubble" TEXT="Para visualiza&#xe7;&#xe3;o">
+<cloud/>
+</node>
+<node CREATED="1366487421810" FOLDED="true" ID="ID_470363954" MODIFIED="1366888989547" STYLE="bubble" TEXT="WMSGetFeatureInfo">
+<node CREATED="1361055375078" ID="ID_969242711" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/getfeatureinfo-control.html" MODIFIED="1366888973565" STYLE="bubble" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Getfeatureinfo-control"/>
+</node>
+<node CREATED="1366585622257" FOLDED="true" ID="ID_1143624427" MODIFIED="1366888989547" STYLE="bubble" TEXT="WFS GetFeature">
+<node CREATED="1366666669798" ID="ID_1717599131" MODIFIED="1366888973565" STYLE="bubble" TEXT="protocol: OpenLayers.Protocol.WFS.fromWMSLayer(layer)"/>
+<node CREATED="1366666979727" ID="ID_960156364" MODIFIED="1366888973565" STYLE="bubble" TEXT="http://localhost:8089/geoserver/www/openlayers2/examples/getfeature-wfs.html">
+<icon BUILTIN="attach"/>
+</node>
+<node CREATED="1366666990047" ID="ID_910610329" MODIFIED="1366888973565" STYLE="bubble" TEXT="http://openlayers.org/dev/examples/getfeature-wfs.html"/>
+</node>
+<node CREATED="1366487421810" FOLDED="true" ID="ID_1442872377" MODIFIED="1366888989547" STYLE="bubble" TEXT="SelectFeature (Vector)">
+<node CREATED="1361062919747" ID="ID_648920800" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/highlight-feature.html" MODIFIED="1366888973566" STYLE="bubble" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Highlight-feature"/>
+</node>
+</node>
+<node CREATED="1366487421812" FOLDED="true" ID="ID_326245597" MODIFIED="1366888989548" STYLE="bubble" TEXT="Ferramentas do mapa">
+<node CREATED="1366585343396" ID="ID_1974188790" MODIFIED="1366888973566" STYLE="bubble" TEXT="Intera&#xe7;&#xe3;o/Edi&#x107;&#xe3;o"/>
+<node CREATED="1366487421812" FOLDED="true" ID="ID_201728548" MODIFIED="1366888989547" STYLE="bubble" TEXT="LayerSwitcher (mostrar/ocultar)">
+<node CREATED="1366594212414" ID="ID_1835880075" MODIFIED="1366888973566" STYLE="bubble" TEXT="exemplo google all overlays"/>
+</node>
+<node CREATED="1366487421812" ID="ID_1802631564" MODIFIED="1366888973566" STYLE="bubble" TEXT="ZoomIn"/>
+<node CREATED="1366487421813" ID="ID_1967109691" MODIFIED="1366888973566" STYLE="bubble" TEXT="DrawFeature (cria&#xe7;&#xe3;o)"/>
+<node CREATED="1354056872401" FOLDED="true" ID="ID_953574917" MODIFIED="1366888989548" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="attach"/>
+<node CREATED="1361157020805" ID="ID_932144189" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/highlight-feature.html" MODIFIED="1366888973566" STYLE="bubble" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Highlight-feature"/>
+</node>
+</node>
+</node>
+<node CREATED="1366585422785" FOLDED="true" ID="ID_917721821" MODIFIED="1366888973566" POSITION="left" STYLE="bubble" TEXT="Formatos vetoriais">
+<node CREATED="1366585437285" MODIFIED="1366667753689" TEXT="GML (padr&#xe3;o)"/>
+<node CREATED="1366585431445" FOLDED="true" ID="ID_1227751075" MODIFIED="1366888989548" TEXT="GeoJson">
+<node CREATED="1366667765990" MODIFIED="1366667770089" TEXT="no protocol"/>
+<node CREATED="1366667756280" MODIFIED="1366667756280" TEXT="outputFormat: &quot;json&quot;,"/>
+<node CREATED="1366667756281" MODIFIED="1366667756281" TEXT="readFormat: new OpenLayers.Format.GeoJSON(),"/>
+</node>
+<node CREATED="1366585515857" FOLDED="true" ID="ID_1999431320" MODIFIED="1366888989548" TEXT="Exemplo">
+<icon BUILTIN="attach"/>
+<node CREATED="1360965720997" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/dwfs-states.html" MODIFIED="1360965720997" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Dwfs-states"/>
+</node>
+</node>
+<node CREATED="1366487429103" FOLDED="true" ID="ID_313000301" MODIFIED="1366888989549" POSITION="left" STYLE="bubble" TEXT="Conceitos - Strategy">
+<node CREATED="1366487429104" FOLDED="true" ID="ID_295809719" MODIFIED="1366888989548" STYLE="bubble" TEXT="Requisi&#xe7;&#xe3;o">
+<node CREATED="1366487429104" ID="ID_193982054" MODIFIED="1366888973566" STYLE="bubble" TEXT="BBOX (&#xe1;rea vis&#xed;vel do mapa)"/>
+<node CREATED="1366487429104" ID="ID_702248205" MODIFIED="1366888973566" STYLE="bubble" TEXT="Fixed (N&#xe3;o se for zoom In)"/>
+<node CREATED="1366668266906" FOLDED="true" ID="ID_754515952" MODIFIED="1366888989548" STYLE="bubble" TEXT="Outros">
+<node CREATED="1366668272778" ID="ID_1629255376" MODIFIED="1366888973567" STYLE="bubble" TEXT="Refresh (autom&#xe1;tico)"/>
+<node CREATED="1366668316410" ID="ID_761813778" MODIFIED="1366888973567" STYLE="bubble" TEXT="Paging (de tanto em tanto)"/>
+</node>
+<node CREATED="1366594276574" FOLDED="true" ID="ID_625037372" MODIFIED="1366888989548" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="attach"/>
+<node CREATED="1360965720997" ID="ID_870715898" LINK="http://localhost:8089/geoserver/www/openlayers2/examples/dwfs-states.html" MODIFIED="1366888973567" STYLE="bubble" TEXT="localhost:8089 &gt; Geoserver &gt; Www &gt; Openlayers2 &gt; Examples &gt; Dwfs-states"/>
+</node>
+</node>
+<node CREATED="1366487429105" FOLDED="true" ID="ID_1733961903" MODIFIED="1366888989549" STYLE="bubble" TEXT="Renderiza&#xe7;&#xe3;o">
+<node CREATED="1366487429105" ID="ID_1091827027" MODIFIED="1366888973567" STYLE="bubble" TEXT="Cluster (agrupamento de pontos)"/>
+<node CREATED="1366594297813" FOLDED="true" ID="ID_1207955684" MODIFIED="1366888989549" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="attach"/>
+<node CREATED="1366668571534" FOLDED="true" ID="ID_995392642" MODIFIED="1366888989548" STYLE="bubble" TEXT="Flickr (Format)">
+<node CREATED="1366668176969" ID="ID_1523938442" MODIFIED="1366888973567" STYLE="bubble" TEXT="http://openlayers.org/dev/examples/strategy-cluster.html"/>
+</node>
+<node CREATED="1366682310839" ID="ID_832600966" MODIFIED="1366888973567" STYLE="bubble" TEXT="http://localhost:8089/geoserver/www/openlayers2/examples/strategy-cluster.html"/>
+</node>
+</node>
+</node>
+<node CREATED="1366585466946" FOLDED="true" ID="ID_667393622" MODIFIED="1366888989549" POSITION="left" STYLE="bubble" TEXT="Estilos">
+<node CREATED="1366586926261" ID="ID_315162899" MODIFIED="1366888973567" STYLE="bubble" TEXT="Est&#xe1;ticos e baseados em atributos"/>
+<node CREATED="1366585470652" ID="ID_845322157" MODIFIED="1366888973567" STYLE="bubble" TEXT="Cluster tamb&#xe9;m trabalha com estilo"/>
+<node CREATED="1366586909685" ID="ID_153349779" MODIFIED="1366888973567" STYLE="bubble" TEXT="Exemplos">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1366594586545" FOLDED="true" ID="ID_1180312652" MODIFIED="1366888989549" POSITION="left" STYLE="bubble" TEXT="Utilit&#xe1;rios">
+<node CREATED="1366594602630" FOLDED="true" ID="ID_457675975" MODIFIED="1366888989549" STYLE="bubble" TEXT="reproje&#xe7;&#xe3;o">
+<node CREATED="1366594613882" ID="ID_183909371" MODIFIED="1366888973568" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1366585730511" FOLDED="true" ID="ID_1572497763" MODIFIED="1366888989549" POSITION="left" STYLE="bubble" TEXT="Integra&#xe7;&#xe3;o com jQuery e ExtJS">
+<node CREATED="1366594728567" ID="ID_356585105" MODIFIED="1366888973568" STYLE="bubble" TEXT="Exemplo">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+</map>
